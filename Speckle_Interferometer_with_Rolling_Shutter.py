d0465d971c166b09b7d0223b51bd88d30c4a5328
Exkalamity <kylelam@ucsb.edu>
2021-07-24 16:17:02 -0700

    Polished Rolling Shutter Image Generator

--- Speckle_Interferometer_with_Rolling_Shutter.py
+++ Speckle_Interferometer_with_Rolling_Shutter.py
@@ -1,9 +1,4 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[1]:
-
-
+from scipy.ndimage import gaussian_filter
 import numpy  as np
 import matplotlib.pyplot as plt
 import time
@@ -13,158 +8,143 @@ from hcipy import *
 import os.path
 from astropy.io import fits
 
+### INPUT PARAMETERS BEGIN HERE ###
 
-# ## Initial Values for Rolling Shutter
+## Metadata
+name = "test6.fits" # filename.fits
+overwrite = True # True if you wish to overwrite files with the same name
 
-# In[2]:
+## Initial Values for Rolling Shutter
+fps = 522 # Readout speed of the detector in frames per second
+detector_size = 512 # width of shortest side of detector. Number of rows in the detector
+number_of_subdivisions = 4 # Number of rows that subdivide the detector to simulate a rolling shutter.
 
-#Readout speed of the detector in frames per second
-fps = 522
-# width of shortest side of detector. Number of rows in the detector
-detector_size = 512 
-#Number of rows that subdivide the detector to simulate a rolling shutter. 1 = global shutter. Detector size = most realistic and computationally expensive
-number_of_subdivisions = 4
+## Properties of the Focal Grid
+q=2 # Number of pixels per resolution element
+nairy = 200 #The spatial extent of the grid radius in resolution elements (=lambda f/D)
 
-#Properties of the Focal Grid
-q=2
-nairy = 200
+## Exposure time and total number of exposures
+exposure_time = 0.01 # Exposure time in seconds
+exposure_total = 3 # Total number of exposures
 
-# Exposure time and total number of exposures
-exposure_time = 0.01 # (seconds)
-exposure_total = 3
-
-# ## Setting Up the Atmosphere
-# seeing estimated from the following source: 
-# https://mthamilton.ucolick.org/techdocs/MH_weather/obstats/avg_seeing.html
-
-# In[5]:
+## Setting Up the Atmosphere
 seeing = 1.75
-outer_scale = 40. # (meter) --> GUESS, NEEDS REFINING
-velocity = 20. # (m/s) --> GUESS, NEEDS REFINING
+outer_scale = 40. # (meter) 
+velocity = 20. # (m/s) 
                                     
-# ## Telescope Setup
-# Starting with the Magellan pupil (scaled to 3.048 m) till we get a description of the Shane pupil 
-
-# In[3]:
-
-
+## Setting up the telescope
 pupil_diameter = 3.048 # (meter)
-collecting_area = np.pi * (3.048**2 - 0.9779**2)
-
 f_number = 13 # effective focal ratio
-effective_focal_length = pupil_diameter * f_number # (meter)
+grid_size = 256 # Number of pixels per dimension
+filter_name = 'V' # Name of filter
+telescope_pupil_generator = make_lick_aperture()
 
-filter_name = 'V'
+## Setting up the noisy detector
+dark_current = 0 # Dark Current
+read_noise = 0 # Read Noise
+flat_field = 0 # Flat Field
+photon_noise = True # Photon Noise
+
+## Add a Primary and Companion
+# Primary parameters
+mag = 5 # Magnitude in the band of interest
+stokes_vector= [1.,0.,0.,0.] # I, Q, U, V
+# Companion parameters
+contrast = 0.
+stokes_ps = [1.,0.,0.,0.] # I, Q, U, V
+angular_separation= 2 # Lambda/D
+
+### INPUT PARAMETERS END HERE ###
+
+## Checking to see if filename exists
+if os.path.isfile(name):
+    if overwrite:
+        print("File name already exists. Preparing to overwrite.") 
+        os.remove(name)
+    else:
+        print("Error, file name already exists. Overwrite was not allowed.")
+        print("Exiting program...")
+        quit()
+
+## Some math to define additional parameters
+collecting_area = np.pi * (3.048**2 - 0.9779**2)
+effective_focal_length = pupil_diameter * f_number # (meter)
 wavelength = filters[filter_name]['lambda_eff'] * 1e-6 # (meter)
 
-
-# In[4]:
-
-
-# Generating the pupil grid
-grid_size = 256
-
+## Generating the pupil grid
+print("Generating the pupil grid")
 pupil_grid = make_pupil_grid(grid_size, diameter=pupil_diameter)
 
-# Adjust spiders to represent Shane pupil
-telescope_pupil_generator = make_lick_aperture()
+## Adjust spiders to represent Shane pupil
+print("Generating the telescope pupil")
 telescope_pupil = telescope_pupil_generator(pupil_grid)
 
-# In[6]:
-fried_parameter = seeing_to_fried_parameter(seeing, wavelength)
-                             
+## Generating the atmosphere
+print("Generating the atmosphere")
+fried_parameter = seeing_to_fried_parameter(seeing, wavelength)                             
 Cn_squared = Cn_squared_from_fried_parameter(fried_parameter, wavelength)
-
 tau0 = 0.314 * fried_parameter/velocity
-
-# Generating phase screens
 layer = InfiniteAtmosphericLayer(pupil_grid, Cn_squared, outer_scale, velocity)
 
-phase_screen_phase = layer.phase_for(wavelength) # (radian)
-phase_screen_opd = phase_screen_phase * (wavelength / (2 * np.pi)) * 1e6
-
-
-
-# In[7]:
-# Propagating to focal plane
-
+##Generating the focal grid
+print("Generating the focal grid")
 focal_grid = make_focal_grid(q=q, 
                              num_airy=nairy,
                              pupil_diameter=pupil_diameter,
                              focal_length = effective_focal_length,
                              reference_wavelength=wavelength)
 
+## Generating the propagator
+print("Generating the propagator")
 prop = FraunhoferPropagator(pupil_grid, focal_grid, 
                             focal_length=effective_focal_length)
 
-
-# ## Add a Primary and Companion
-
-# In[8]:
-
-
-# Primary parameters
-mag = 5 #Vega magnitude in the band of interest
-stokes_vector= [1.,0.,0.,0.] #I, Q, U, V
-
-# Companion parameters
-contrast = 0.
-stokes_ps = [1.,0.,0.,0.] #I, Q, U, V
-angular_separation= 2 #Lambda/D
-
-
-# ### Simulating Image Data
-
-# In[9]:
-
-
-from scipy.ndimage import gaussian_filter
-
-def center_image(img, gsigma, cpix):
-#   [img] - image array
-#   [gsigma] - standard deviation for Gaussian kernel
-#   [cpix] - number of pixels in output image 
-    im_o_g = gaussian_filter(img, sigma=gsigma)
-    maximum = np.where(im_o_g == np.max(im_o_g))
-    #print(maximum)
-    #print(type(img))
-    #print(type(im_o_g))
-    #imshow_field(img)
-    #plt.imshow(im_o_g)
-    x1 = int(maximum[0] - (cpix/2))
-    x2 = int(maximum[0] + (cpix/2))
-    
-    y1 = int(maximum[1] - (cpix/2))
-    y2 = int(maximum[1] + (cpix/2))
-    
-    center_image = img[x1:x2, y1:y2]
-    return(center_image)
-
-
-# In[10]:
-
-
+## Generating wavefront of primary and companion
+print("Generating wavefront of primary and companion")
 pupil_wavefront = Wavefront(telescope_pupil, wavelength,
                             input_stokes_vector=stokes_vector)
 pupil_wavefront.total_power = number_of_photons(mag,filter_name,collecting_area,) #In photons/s
-
-
 wf_planet = Wavefront(telescope_pupil*np.exp(4j*np.pi*pupil_grid.x*angular_separation/pupil_diameter),
                       wavelength,
                       input_stokes_vector=stokes_ps)
 wf_planet.total_power = contrast * number_of_photons(mag,filter_name,collecting_area,) # (photons/s)
 
-
-# In[11]:
-##Create the FITS file
+## Create the FITS file
+print("Creating the FITS file")
 hdr = fits.Header()
-hdr['Title'] = 'Speckle Rolling Shutter Test Images'
-hdr['COMMENT'] = "This fits contains image data for testing a rolling shutter for speckle imaging. Written by Kyle Lam."
+hdr['Title'] = name
+hdr['Author'] = "Written by Kyle Lam."
+hdr['FPS'] = str(fps) + " # Readout speed of the detector in fps"
+hdr['DetSize'] = str(detector_size) + " # width of shortest side of detector."
+hdr['NSubdivi'] = str(number_of_subdivisions) + " # Number of Subdivisions"
+hdr['q'] = str(q) + " # Number of pixels per resolution element"
+hdr['NAiry'] = str(nairy) + " # The spatial extent of the grid radius in resolution elements"
+hdr['ExpoTime'] = str(exposure_time) + " # Exposure time in seconds"
+hdr['NumExpos'] = str(exposure_total) + " # Total Number of Exposures"
+hdr['Seeing'] = str(seeing) + " # Seeing"
+hdr['OutScale'] = str(outer_scale) + " # Outer Scale"
+hdr['Velocity'] = str(velocity) + " # Wind Velocity"
+hdr['PupDiamt'] = str(pupil_diameter) + " # Pupil Diameter"
+hdr['FNum'] = str(f_number) + " # F Number"
+hdr['GridSize'] = str(grid_size) + " # Grid Size"
+hdr['FiltName'] = str(filter_name) + " # Filter Name"
+hdr['DarkCurr'] = str(dark_current) + " # Dark Current"
+hdr['RdNoise'] = str(read_noise) + " # Read Noise"
+hdr['FltField'] = str(flat_field) + "# Flat Field"
+hdr['PhtNoise'] = str(photon_noise) + " # Photon Noise"
+hdr['PriMag'] = str(mag) + " # Magnitude of Primary"
+hdr['PriStoke'] = str(stokes_vector) + " # Stokes Vector of Primary"
+hdr['Contrast'] = str(contrast) + " # Companion Contrast"
+hdr['ComStoke'] = str(stokes_ps) + " # Stokes Vector of Companion"
+hdr['AngSep'] = str(angular_separation) + " # Angular Separation"
 empty_primary = fits.PrimaryHDU(header=hdr)
 hdul = fits.HDUList([empty_primary])
 
-##Create the rolling shutter image
-detector = NoisyDetector(focal_grid, dark_current_rate=0, read_noise=0, flat_field=0, include_photon_noise=True)
+## Create the rolling shutter image
+print("Generating the rolling shutter image")
+detector = NoisyDetector(focal_grid, dark_current_rate= dark_current, 
+                        read_noise=read_noise, flat_field=flat_field, 
+                        include_photon_noise=photon_noise)
 number_of_rows = int(np.sqrt(focal_grid.size))# Width of the focal plane. Number of in rows the FOCAL PLANE.
 row_layout = np.linspace(0, number_of_rows, number_of_subdivisions, dtype = "int")
 row_readout_rate = 1/fps/detector_size
@@ -190,17 +170,9 @@ for i in range(exposure_total):
 
 
 ##Write the FITS file\
-while True:
-    print("Type a name in for this fits file.")
-    name = input('Name:')
-    name = name + '.fits'
-    command = 0
-    if os.path.isfile(name):
-        print()
-        print("File name already exists")
-    else:
-        break
-hdul.writeto(name)
 print()
-print("FITS file generated.")  
+print("Writing the FITS file")
+hdul.writeto(name)
+print("FITS file", name,  "generated.")
+
 
